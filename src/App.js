2cab4c3ccdc24320c944ce8bea2852a9b7042911
lokeshloki3 <lokesh1992p@gmail.com>
2024-09-22 22:51:10 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
       </div>
 
       <Routes>
-        <Route path='/' element={<Home/>} />
+        <Route path='/shoppingcartreact' element={<Home/>} />
         <Route path='/cart' element={<Cart/>} />
       </Routes>
     </div>
