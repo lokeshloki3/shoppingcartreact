bd40e4494439176aa2deeb3f85d0001956c49c3e
lokeshloki3 <lokesh1992p@gmail.com>
2024-09-23 00:00:39 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ function App() {
 
       <Routes>
         <Route path='/' element={<Home/>} />
+        <Route path='/shoppingcartreact' element={<Home/>} />
         <Route path='/cart' element={<Cart/>} />
       </Routes>
     </div>
